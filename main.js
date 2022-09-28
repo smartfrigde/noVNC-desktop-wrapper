f471ba5c433eeb6e6c857ae31961ed51a3a3b643
smartfrigde <37928912+smartfrigde@users.noreply.github.com>
2022-09-28 10:05:49 +0000

    Prototype

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadUrl(process.env.INSTANCEURL)
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
